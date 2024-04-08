a212f70979c1237c3a31e080ec4162ca62f0596a
ben demetrius <ben.demetrius@liferay.com>
2024-04-08 18:31:58 +0530

    fix: removed unnecessary variable

--- src/assets/js/removePeople.js
+++ src/assets/js/removePeople.js
@@ -5,7 +5,6 @@ export const removePeople = async () => {
   let data = await get();
 
   for (let i = 0; i < data.length; i++) {
-    let id = data[i].id;
-    remove(id);
+    remove(data[i].id);
   }
 };
