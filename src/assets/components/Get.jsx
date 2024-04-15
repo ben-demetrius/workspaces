0c5bc2cefa4a1a85e9fc49f8e5094a057f3997e2
ben demetrius <ben.demetrius@liferay.com>
2024-04-15 14:17:55 +0530

    feat: mapped get with cards

--- src/assets/components/Get.jsx
+++ src/assets/components/Get.jsx
@@ -2,22 +2,36 @@ import React, { useEffect, useState } from "react";
 import { get } from "../js/httpRequests.js";
 
 const Get = () => {
-  const [data, setData] = useState("");
-
+  const [data, setData] = useState([]);
   const fetchData = async () => {
     const fetchdata = await get();
     //console.log(fetchdata);
-    setData(fetchdata[fetchdata.length - 1].firstname);
+    setData(fetchdata);
   };
 
   useEffect(() => {
-    //    console.log("holla Ben");
+    // setData(fetchData);
     fetchData();
-  }, [data]);
+  }, []);
+
+  // console.log(data[1].firstname);
+  console.log(data);
+
+  const cards = (data) => {
+    return (
+      <div>
+        <p>{data.firstname}</p>
+        <p>{data.lastname}</p>
+        <p>{data.yearsLeft}</p>
+      </div>
+    );
+  };
 
   return (
-    <div div className="get">
-      Hello {data}
+    <div>
+      {data.map((data, index) => {
+        return <div key={index}>{cards(data)}</div>;
+      })}
     </div>
   );
 };
