07f8f09c15208b0fb2d3cece2b3ca10785cb973a
ben demetrius <ben.demetrius@liferay.com>
2024-06-24 17:09:40 +0530

    chore: commented get and post

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./assets/styles/App.scss";
-import Post from "./assets/components/Post";
-import Get from "./assets/components/Get";
+// import Post from "./assets/components/Post";
+// import Get from "./assets/components/Get";
 
 const App = () => {
   return (
     <div className="App">
-      <Post />
-      <Get />
+      {/* <Post />
+      <Get /> */}
     </div>
   );
 };
