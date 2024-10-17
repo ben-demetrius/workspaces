a83baec8692b7dfce26e29b7d2ef03bffe256b95
ben demetrius <ben.demetrius@liferay.com>
2024-10-17 10:51:14 +0530

    fix: fixed the form that triggers constructor.

--- src/assets/components/miscellaneous/Post.jsx
+++ src/assets/components/miscellaneous/Post.jsx
@@ -2,18 +2,32 @@ import React, { useState } from "react";
 import Person from "../../js/Person";
 
 const Post = () => {
-  const [age, setAge] = useState("");
+  const [age, setAge] = useState(0);
+  const [name, setName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const tempObj = new Person({ firstName: name, age: age });
+    console.log(tempObj);
+  };
 
   return (
-    <form className="Post">
+    <form className="Post" onSubmit={handleSubmit}>
       <input
         type="int"
+        placeholder="Age"
+        id="age"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="number"
         placeholder="Age"
         id="age"
         value={age}
         onChange={(e) => setAge(e.target.value)}
       />
-      <button onClick={() => new Person(age)}>POST</button>
+      <button type="submit">POST</button>
     </form>
   );
 };
