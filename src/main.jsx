1d1d79e23a4acfc49da23bf234d8159572db43ad
ben demetrius <ben.demetrius@liferay.com>
2024-11-06 12:24:20 +0530

    chore: made it liferay compatible.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,30 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { render, unmountComponentAtNode } from "react-dom";
+
+class WebComponent extends HTMLElement {
+  connectedCallback() {
+    render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+      this
+    );
+  }
+
+  disconnectedCallback() {
+    unmountComponentAtNode(this);
+  }
+}
+
+const ELEMENT_NAME = "react-template";
+
+if (customElements.get(ELEMENT_NAME)) {
+  // eslint-disable-next-line no-console
+  console.log(
+    `Skipping registration for <${ELEMENT_NAME}> (already registered)`
+  );
+} else {
+  customElements.define(ELEMENT_NAME, WebComponent);
+}
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "/o/react-template",
+  build: {
+    outDir: "./vite-build",
+    rollupOptions: {
+      external: ["react", "react-dom", /^(?!@clayui\/css)@clayui.*$/],
+    },
+  },
+  plugins: [
+    react({
+      jsxRuntime: "classic",
+    }),
+  ],
+});
